e4c0aea06f4d43c66ec8b20edf2e2787c0c05ce0
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-21 15:23:28 +0530

    Update string_calculator.cpp

--- string_calculator.cpp
+++ string_calculator.cpp
@@ -1,7 +1,6 @@
 #include <stdexcept>
 #include <string>
 
-int add(const std::string& str_input) 
-{
+int add(const std::string& str_input) {
     throw std::logic_error("This method is not implemented yet");
 }
