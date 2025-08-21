7460e2d596cd97655d7333a20b0dc33fbc286136
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-21 15:05:51 +0530

    Create string_caculator_tests.cpp

---
+++ string_caculator_tests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+TEST(TestStringCalculator, EmptyStringReturnsZero) {
+    // Arrange
+    std::string input = "";
+    int expected_value = 0;
+
+    // Act
+    int actual_value = add(input);
+
+    // Assert
+    EXPECT_EQ(actual_value, expected_value);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
