cb2f2274b07455f41585e5a28fa4b47801032436
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-25 14:30:32 +0530

    Update string_calculator.h

--- string_calculator.h
+++ string_calculator.h
@@ -6,18 +6,21 @@
 #include <stdexcept>
 
 class NegativeNumberException : public std::runtime_error {
-public:
-    explicit NegativeNumberException(const std::string& msg) : std::runtime_error(msg) {}
+ public:
+  explicit NegativeNumberException(const std::string& msg)
+      : std::runtime_error(msg) {}
 };
 
 class StringCalculator {
-public:
-    int Add(const std::string& numbers);
+ public:
+  int Add(const std::string& numbers);
 
-private:
-    std::vector<std::string> ParseDelimiters(const std::string& input, std::string& numbersPart);
-    std::vector<int> ExtractNumbers(const std::string& numbers, const std::vector<std::string>& delimiters);
-    int SumNumbers(const std::vector<int>& numbers);
+ private:
+  std::vector<std::string> ParseDelimiters(const std::string& input,
+                                           std::string& numbersPart);
+  std::vector<int> ExtractNumbers(const std::string& numbers,
+                                  const std::vector<std::string>& delimiters);
+  int SumNumbers(const std::vector<int>& numbers);
 };
 
-#endif
+#endif  // STRING_CALCULATOR_H_
