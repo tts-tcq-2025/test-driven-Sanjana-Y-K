03426c1bb1c66f6fa878fbbcc43f98f24a97483c
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-25 12:18:45 +0530

    Update string_calculator_tests.cpp

--- string_calculator_tests.cpp
+++ string_calculator_tests.cpp
@@ -1,21 +1,70 @@
-#include <string>
-#include "string_calculator.cpp"
-#include string_calculator.h
-#include <gtest/gtest.h>
+#include "string_calculator.h"
+#include <cassert>
+#include <iostream>
 
-TEST(TestStringCalculator, EmptyStringReturnsZero) {
-    // Arrange
-    std::string input = "";
-    int expected_value = 0;
-
-    // Act
-    int actual_value = add(input);
-
-    // Assert
-    EXPECT_EQ(actual_value, expected_value);
+void TestEmptyStringReturnsZero() {
+    StringCalculator calc;
+    assert(calc.Add("") == 0);
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+void TestSingleNumberReturnsItself() {
+    StringCalculator calc;
+    assert(calc.Add("1") == 1);
+}
+
+void TestTwoNumbersCommaSeparated() {
+    StringCalculator calc;
+    assert(calc.Add("1,2") == 3);
+}
+
+void TestUnknownAmountOfNumbers() {
+    StringCalculator calc;
+    assert(calc.Add("1,2,3,4,5") == 15);
+}
+
+void TestNewLineDelimiter() {
+    StringCalculator calc;
+    assert(calc.Add("1\n2,3") == 6);
+}
+
+void TestCustomSingleCharDelimiter() {
+    StringCalculator calc;
+    assert(calc.Add("//;\n1;2") == 3);
+}
+
+void TestNegativeNumbersThrowException() {
+    StringCalculator calc;
+    try {
+        calc.Add("1,-2,-3");
+        assert(false); // Should not reach here
+    } catch (const NegativeNumberException& e) {
+        std::string expected = "negatives not allowed: -2, -3";
+        assert(std::string(e.what()) == expected);
+    }
+}
+
+void TestIgnoreNumbersGreaterThan1000() {
+    StringCalculator calc;
+    assert(calc.Add("2,1001") == 2);
+}
+
+void TestDelimitersOfAnyLength() {
+    StringCalculator calc;
+
+    assert(calc.Add("//[***]\n12***3") == 15);
+}
+
+int main() {
+    TestEmptyStringReturnsZero();
+    TestSingleNumberReturnsItself();
+    TestTwoNumbersCommaSeparated();
+    TestUnknownAmountOfNumbers();
+    TestNewLineDelimiter();
+    TestCustomSingleCharDelimiter();
+    TestNegativeNumbersThrowException();
+    TestIgnoreNumbersGreaterThan1000();
+    TestDelimitersOfAnyLength();
+
+    std::cout << "All tests passed!\n";
+    return 0;
 }
