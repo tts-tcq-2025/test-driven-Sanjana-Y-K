aca0684f3906ad495c8332133e3c907a2354596b
Sanjana-Y-K <sanjanayk7102001@gmail.com>
2025-08-28 11:24:57 +0530

    Update string_calculator.h

--- string_calculator.h
+++ string_calculator.h
@@ -1,18 +1,20 @@
-#ifndef STRING_CALCULATOR_H
-#define STRING_CALCULATOR_H
+#ifndef STRING_CALCULATOR_H_
+#define STRING_CALCULATOR_H_
 
 #include <string>
 #include <vector>
 
 class StringCalculator {
-public:
-    int Add(const std::string& numbers);
+ public:
+  int Add(const std::string& numbers);
 
-private:
-    std::string ExtractDelimiter(std::string& numbers);
-    std::vector<std::string> Split(const std::string& input, const std::string& delimiter);
-    std::vector<int> ParseNumbers(const std::string& numbers, const std::string& delimiter);
-    void CheckNegatives(const std::vector<int>& nums);
+ private:
+  std::string ExtractDelimiter(std::string& numbers);
+  std::vector<std::string> Split(const std::string& input,
+                                 const std::string& delimiter_regex);
+  std::vector<int> ParseNumbers(const std::string& numbers,
+                                const std::string& delimiter_regex);
+  void CheckNegatives(const std::vector<int>& nums);
 };
 
-#endif  // STRING_CALCULATOR_H
+#endif  
